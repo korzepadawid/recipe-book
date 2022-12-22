abc8e509fa3c593adc792e9d5081b13018607754
korzepadawid <korzepadawid@yahoo.com>
2022-12-22 15:52:00 +0100

    added swagger and mongodb config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+/**
+ * Configures Swagger and returns the config.
+ * @returns The Swagger config.
+ */
+const configSwagger = (): Omit<OpenAPIObject, 'paths'> =>
+  new DocumentBuilder()
+    .setTitle('My social media platform')
+    .setDescription('The endpoints documentation with the OpenAPI')
+    .setVersion('1.0')
+    .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefixes
+  app.setGlobalPrefix('/api/v1');
+
+  // Swagger
+  const document = SwaggerModule.createDocument(app, configSwagger());
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
